ac61476ea69a53b7999715d4c1c9ab55f57ccff2
TungNT140601 <tungnt14062001@gmail.com>
2023-07-10 10:36:44 +0700

    up enviroment

--- HRM_MVC/Program.cs
+++ HRM_MVC/Program.cs
@@ -1,8 +1,17 @@
+using DataAccess.EmployeeRepositories;
+using Microsoft.EntityFrameworkCore;
+using Model.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<HRM_SWD392Context>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("HRM_SWD392"));
+});
+builder.Services.AddSession();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,7 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
