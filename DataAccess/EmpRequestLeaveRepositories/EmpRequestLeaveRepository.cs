efa2b85b42a80fa19acb4d1ba7a1df54c6b1b9c1
tainguyen58ntt <tainguyen58ntt@gmail.com>
2023-07-14 15:35:39 +0700

    emp - leave - registration

---
+++ DataAccess/EmpRequestLeaveRepositories/EmpRequestLeaveRepository.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Data;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EmpRequestLeaveRepositories
+{
+    public interface IEmpRequestLeaveRepository
+    {
+        List<LeaveRequest>? GetAll();
+
+        List<LeaveRequest>? GetByName(string nameUser);
+        List<LeaveRequest> GetByNameAndHrId(string nameUser, int hrId);
+        List<LeaveRequest>? GetByUserId(string userId);
+        List<LeaveRequest>? GetByHrId(int? hrId);
+        LeaveRequest GetById(int? id);
+
+
+
+        void Update(LeaveRequest requestLeave);
+        LeaveRequest saveLeaveRequest(LeaveRequest requestLeave);
+    }
+
+
+    public class EmpRequestLeaveRepository : IEmpRequestLeaveRepository
+    {
+        protected readonly HRM_SWD392Context _context;
+        public EmpRequestLeaveRepository(HRM_SWD392Context context)
+        {
+            if (_context == null)
+            {
+                _context = context;
+            }
+        }
+        public EmpRequestLeaveRepository()
+        {
+            if (_context == null)
+            {
+                _context = new HRM_SWD392Context();
+            }
+        }
+        public LeaveRequest saveLeaveRequest(LeaveRequest requestLeave)
+        {
+            try
+            {
+
+
+                _context.LeaveRequests.Add(requestLeave);
+                _context.SaveChanges();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return requestLeave;
+        }
+
+        public List<LeaveRequest>? GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LeaveRequest>? GetByHrId(int? hrId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveRequest GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LeaveRequest>? GetByName(string nameUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LeaveRequest> GetByNameAndHrId(string nameUser, int hrId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LeaveRequest>? GetByUserId(string userId)
+        {
+            var requestLeaves = _context.LeaveRequests.Include(r => r.Hr).Where(c => c.EmployeeId == userId).ToList();
+            return requestLeaves;
+        }
+
+        public void Update(LeaveRequest requestLeave)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/EmpResignationRequestsRepositories/EmpResignationRequestsRepository.cs
@@ -0,0 +1,53 @@
+﻿using Model.Data;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EmpResignationRequestsRepositories
+{
+
+    public interface IEmpResignationRequestsRepository
+    {
+        ResignationRequest saveResgination(ResignationRequest resignationRequest);
+    }
+    public class EmpResignationRequestsRepository : IEmpResignationRequestsRepository
+    {
+
+        protected readonly HRM_SWD392Context _context;
+        public EmpResignationRequestsRepository(HRM_SWD392Context context)
+        {
+            if (_context == null)
+            {
+                _context = context;
+            }
+        }
+        public EmpResignationRequestsRepository()
+        {
+            if (_context == null)
+            {
+                _context = new HRM_SWD392Context();
+            }
+        }
+        public ResignationRequest saveResgination(ResignationRequest resignationRequest)
+        {
+            try
+            {
+
+
+                _context.ResignationRequests.Add(resignationRequest);
+                _context.SaveChanges();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return resignationRequest;
+        }
+    }
+}
