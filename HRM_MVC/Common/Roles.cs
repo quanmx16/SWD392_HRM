d3848963317240dbca0c36117ead10a7a897f349
mai xuan quan <maixuanquan.mxq@gmail.com>
2023-07-16 21:08:25 +0700

    add homes

---
+++ HRM_MVC/Common/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace HRM_MVC.Common
+{
+    public class Roles
+    {
+        public static String ROLE_EMPLOYEE = "Employee";
+        public static String ROLE_HR = "HR";
+        public static String ROLE_HR_MANAGER = "HRManager";
+
+    }
+}
